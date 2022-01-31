40ed47c65f9e299c6e80d853623e87b73a398197
Sebastian Acosta <sebastian.acosta@grantitan.com>
2022-01-31 08:55:45 -0500

    Creacion de PeliculasApi

---
+++ PeliculasAPI/Repositorio/IRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Repositorio
+{
+   public interface IRepositorio
+    {
+        List<Genero> ObtenerTodosLosGeneros();
+    }
+}
---
+++ PeliculasAPI/Repositorio/RepositorioEnMemoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Repositorio
+{
+    public class RepositorioEnMemoria : IRepositorio
+    {
+
+        private List<Entidades.Genero> _generos;
+        public RepositorioEnMemoria()
+        {
+
+            _generos = new List<Entidades.Genero>()
+            {
+                new Entidades.Genero(){Id = 1, Nombre ="Comedia"},
+                new Entidades.Genero(){Id = 2, Nombre ="Drama"},
+            };
+
+        }
+
+        public List<Entidades.Genero> ObtenerTodosLosGeneros()
+        {
+            return _generos;
+        }
+
+
+    }
+}
