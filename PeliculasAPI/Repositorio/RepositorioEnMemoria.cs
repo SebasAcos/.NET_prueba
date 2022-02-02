cbb8fd4ce0c259e56b24e378c8c61d6c183ead07
Sebastian Acosta <sebastian.acosta@grantitan.com>
2022-02-02 09:49:08 -0500

    prueba

--- PeliculasAPI/Repositorio/RepositorioEnMemoria.cs
+++ PeliculasAPI/Repositorio/RepositorioEnMemoria.cs
@@ -17,14 +17,35 @@ namespace PeliculasAPI.Repositorio
                 new Entidades.Genero(){Id = 1, Nombre ="Comedia"},
                 new Entidades.Genero(){Id = 2, Nombre ="Drama"},
             };
-
+            _guid = Guid.NewGuid();
         }
+
+        public Guid _guid;
 
         public List<Entidades.Genero> ObtenerTodosLosGeneros()
         {
             return _generos;
         }
 
+        //metodo asincrono con async
+        public async Task<Entidades.Genero> obtenerPorId(int Id)
+        {
+            //tiempo de espera para dar resultado
+            await Task.Delay(1); 
 
+            return _generos.FirstOrDefault(x => x.Id == Id);
+        }
+
+        public Guid ObternerGUID()
+        {
+            return _guid;
+        }
+
+        public void CrearGenero(Entidades.Genero genero)
+        {
+            genero.Id = _generos.Count() + 1;
+            _generos.Add(genero);
+        }
+         
     }
 }
